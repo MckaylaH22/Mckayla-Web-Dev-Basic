9b351f53ac7d67060377b77488aadec530e92b7c
MckaylaH22 <156045294+MckaylaH22@users.noreply.github.com>
2024-04-02 20:30:26 -0400

    Declaring alternative text for each image

--- Assignment 4 part 2/main.js
+++ Assignment 4 part 2/main.js
@@ -5,8 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 
 /* Declaring the alternative text for each image file */
+const altTexts = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'A beautiful landscape',
+    'pic3.jpg': 'Adorable kitten playing',
+    'pic4.jpg': 'Stunning sunset over the ocean',
+    'pic5.jpg': 'Delicious plate of food'
+};
+
+
 
 /* Looping through images */
 
