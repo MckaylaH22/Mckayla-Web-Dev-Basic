033add2d98aad5a496e7836caada61b95fbabc21
MckaylaH22 <156045294+MckaylaH22@users.noreply.github.com>
2024-04-03 08:50:39 -0400

    Final commit
    Had to fix some of the code because darkening the image was not working because in the event listener section the newImage was outside of the loop.

--- Assignment 4 part 2/Assignment 4 part 2/main.js
+++ Assignment 4 part 2/Assignment 4 part 2/main.js
@@ -5,14 +5,48 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 
 /* Declaring the alternative text for each image file */
+const altTexts = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'A beautiful landscape',
+    'pic3.jpg': 'Adorable kitten playing',
+    'pic4.jpg': 'Stunning sunset over the ocean',
+    'pic5.jpg': 'Delicious plate of food'
+};
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+imageFilenames.forEach(filename => {
+  const newImage = document.createElement('img');
+  const imagePath = 'images/' + filename; 
+  newImage.setAttribute('src', imagePath); 
+  newImage.setAttribute('alt', altTexts[filename]); 
+  thumbBar.appendChild(newImage);
+  
+  /* Add click event listener to every newImage*/
+  newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+  });
+});
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+
+  if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+  } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+  }
+});
+
+
+
+
